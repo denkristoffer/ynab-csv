abecc61aa6aa45009e4c436d68413eea592ab249
denkristoffer <denkristoffer@users.noreply.github.com>
2021-01-16 19:50:04 +0000

    Deploying to gh-pages from  @ b9366528b982f61f6e2fbf5f980c28aa5a8da11d 🚀

--- _next/static/11ubRCjPulw3THNrTl8-B/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-391094a03668f920c73e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5d1b254cd2949f6e6354.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
