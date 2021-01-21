461e2d78322b51fa76f4148ce46c8c959d1ad222
denkristoffer <denkristoffer@users.noreply.github.com>
2021-01-21 22:13:14 +0100

    feat(download-button): transition on opacity

--- src/components/downloadButton.tsx
+++ src/components/downloadButton.tsx
@@ -4,18 +4,20 @@ import type { MouseEvent } from "react";
 import DownloadIcon from "../components/downloadIcon";
 
 interface DownloadButtonProps {
+  className?: string;
   isActive?: boolean;
   onClick: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
 export default function DownloadButton({
+  className,
   isActive,
   onClick,
 }: DownloadButtonProps): React.ReactElement {
   return (
     <button
       aria-label="Download CSV"
-      className="flex flex-row rounded-md bg-black text-white text-center px-6 py-4 fixed bottom-8 right-8 no-select"
+      className={`flex flex-row rounded-md bg-black text-white text-center px-6 py-4 fixed bottom-8 right-8 no-select transition-opacity ${className}`}
       disabled={isActive}
       onClick={onClick}
     >
